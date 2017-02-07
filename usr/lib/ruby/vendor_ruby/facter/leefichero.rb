80971ef46504cb4c1bac1c31335379266044eaee
algodelinux <algodelinux@gmail.com>
2017-02-07 10:18:25 +0100

    Upgraded sincpuppet to 2.16 version

--- usr/lib/ruby/vendor_ruby/facter/leefichero.rb
+++ usr/lib/ruby/vendor_ruby/facter/leefichero.rb
@@ -1,18 +1,1 @@
-# Archivo /usr/lib/ruby/vendor_ruby/facter/leefichero.rb
-# Se utiliza para leer fichero /etc/escuela2.0 y anadir variables a facter
-
-if File.exists?("/etc/escuela2.0")
-   File.open("/etc/escuela2.0").each do |line|
-      var = nil
-      value = nil
-
-      var = $1 and val = $2 if line =~ /^(.+)=(.+)$/
-
-      if var != nil && val != nil
-         Facter.add(var) do
-            setcode { val }
-         end
-      end
-   end
-end
-####
+../../../../share/linex-ubuntu-puppet/leefichero.rb
